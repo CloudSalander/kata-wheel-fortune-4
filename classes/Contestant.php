52a98a2bb546b46ae2d5f69dcb06d453621a769a
CloudSalander <ruben.as532@gmail.com>
2025-07-23 09:38:35 +0200

    Added single letter validation

--- classes/Contestant.php
+++ classes/Contestant.php
@@ -4,13 +4,18 @@ class Contestant {
     private string $name;
     public int $points;
 
+    const SINGLE_LETTER_PATTERN = "/^[a-zA-Z]$/";
+
     public function __construct(string $name){
         $this->name = $name;
         $this->points = 0;
     }
     public function sayLetter(): string { 
-        //TODO: Check right input here. Just one char. Char must be an allowed letter.
-        return strtoupper(readline());
+        $letter = readline();
+        while(! $this->isValidChar($letter)) {
+            $letter = readline();
+        }
+        return strtoupper($letter);
     }
 
     public function saySolution(): string {
@@ -32,6 +37,10 @@ class Contestant {
 
     public function getScore(): int {
         return $this->points;
+    }
+
+    private function isValidChar(string $char): bool {
+        return preg_match(self::SINGLE_LETTER_PATTERN, $char) === 1;
     }
 }
 
